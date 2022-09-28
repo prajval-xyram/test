6e0029e97c8acb126f09df7c28b153ac973d772e
Prajval <prajval.hl@xyramsoft.com>
2022-09-28 15:40:09 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'arcadia-input-ccda';
+  inputForm = new FormGroup({
+    fileInput: new FormControl(''),
+  });
+  fileExtension: string = '';
+  filesToUpload: any;
+  fd: any;
+
+  constructor(private http: HttpClient) {}
+
+  onFileChange(event: any) {
+    this.fileExtension = '';
+    this.filesToUpload = [];
+
+    if (event.target.files.length) {
+      if (event.target.files[0].name.lastIndexOf('.') !== -1) {
+        this.fileExtension = event.target.files[0].name.substring(
+          event.target.files[0].name.lastIndexOf('.') + 1,
+          event.target.files[0].name.length
+        );
+        if (this.fileExtension.toLowerCase() === 'xlsx') {
+          this.filesToUpload.push(event.target.files[0]);
+          this.inputForm.controls['fileInput'].setValue(
+            this.filesToUpload[0].name
+          );
+        } else {
+          this.fileExtension = '';
+        }
+      } else {
+        this.fileExtension = '';
+      }
+    } else {
+      this.inputForm.controls['fileInput'].setValue('Upload files');
+    }
+  }
+
+  onSubmit() {
+    this.fd = new FormData();
+
+    if (this.fileExtension !== '') {
+      this.fd.append('file', this.filesToUpload[0]);
+    }
+  }
+
+  fileUpload() {
+    this.http.post('/api/generateCcda', this.fd).subscribe((res) => {
+      console.log(res);
+      return res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [ReactiveFormsModule, FormsModule],
 })
-export class AppModule { }
+export class AppModule {}
